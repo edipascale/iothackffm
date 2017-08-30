eeec44eea740f15b43459823b6b3cc4d14d44363
edipascale <dipascae@tcd.ie>
2017-08-30 11:43:55 +0200

    Changed the contract to allow deliverers to specify the price they are willing to perform the shipment for, rather than having the sender specifying it. This means the contract can only be funded after a deliverer has been agreed upon. Updated the README to reflect these changes and to include other small considerations on the limits of the MVP.

--- contracts/PePaDe.sol
+++ contracts/PePaDe.sol
@@ -2,7 +2,13 @@ pragma solidity ^0.4.4;
 
 contract PePaDe {
   enum State {
-    Proposed, Funded, WithOffer, Agreed, Shipping, Delivered, Paid, Expired, Exception
+    Proposed, WithOffer, Agreed, Funded, Shipping, Delivered, Paid, Expired, Exception
+  }
+
+  struct DelivererCandidate {
+    address id;
+    uint proposedFee;
+    uint offerExpiration;
   }
 
   address public sender;
@@ -14,23 +20,26 @@ contract PePaDe {
   string destAddress;
   State public state;
   int maxTemp;
+  int minTemp;
   uint public fee;
   uint deposit;
-  address[] candidates;
+  mapping (uint => DelivererCandidate) candidates;
   uint candidateLen;
   uint creationTime;
   uint expirationTime;
   bool senderConfirmed;
   bool delivererConfirmed;
 
-  event NewCandidate(address deliverCandidate, uint index);
-  event DelivererSelected(address selectedDeliverer);
+  event NewCandidate(uint index, address deliverCandidate, uint proposedFee, uint offerExpiration);
+  event DelivererSelected(address selectedDeliverer, uint fee);
+  event ContractFunded();
   event ParcelShipped();
   event ParcelDelivered();
   event ParcelExpired();
   event FeePaid();
   event InsufficientFunds(uint deficit);
   event TemperatureException(int temperature);
+  event ContractDestroyed();
 
   modifier onlySender {
     require(msg.sender == sender);
@@ -63,43 +72,45 @@ contract PePaDe {
   }
 
   modifier garbageCollectable {
-    require(state == State.Paid || state == State.Expired || state == State.Exception);
+    require(state == State.Paid || state == State.Expired || state == State.Exception
+      || ((state == State.Agreed || state == State.Shipping) && now >= creationTime + expirationTime));
     _;
   }
 
   function PePaDe(address _recipient, string _originAddress,
-           string _destAddress, int _maxTemp, uint _fee, uint _weightInKg,
-           uint _expirationTime) payable {
+           string _destAddress, int _maxTemp, int _minTemp, uint _fee,
+           uint _weightInKg, uint _expirationTime) payable {
     sender = msg.sender;
     recipient = _recipient;
     weightInKg = _weightInKg;
     originAddress = _originAddress;
     destAddress = _destAddress;
     maxTemp = _maxTemp;
-    fee = _fee;
+    minTemp = _minTemp;
     creationTime = now;
     expirationTime = _expirationTime;
     candidateLen = 0;
-    if (msg.value >= fee)
-      state = State.Funded;
-    else
-      state = State.Proposed;
+    state = State.Proposed;
   }
 
   // fallback function; also check if we have now enough funds to start the contract
   function () payable {
-    if (state == State.Proposed && this.balance > fee)
+    if (state == State.Agreed && this.balance > fee) {
       state = State.Funded;
+      ContractFunded();
+    }
   }
 
   // should we ask/check for deposit here?
-  function deliverCandidate() public returns(uint) {
-    require(state == State.Funded || state == State.WithOffer);
+  function deliverCandidate(uint _proposedFee, uint _offerExpiration) public returns(uint) {
+    require(state == State.Proposed || state == State.WithOffer);
     uint index = candidateLen;
-    candidates[index] = msg.sender;
+    candidates[index] = DelivererCandidate({id: msg.sender,
+                                            proposedFee: _proposedFee,
+                                            offerExpiration: _offerExpiration});
     candidateLen += 1;
-    NewCandidate(msg.sender, index);
-    if (state == State.Funded)
+    NewCandidate(index, msg.sender, _proposedFee, _offerExpiration);
+    if (state == State.Proposed)
       state = State.WithOffer;
     return index;
   }
@@ -107,16 +118,22 @@ contract PePaDe {
   function selectDeliverer(uint index) onlySender public {
     require(state == State.WithOffer);
     require(index < candidateLen);
-    deliverer = candidates[index];
+    DelivererCandidate selected = candidates[index];
+    deliverer = selected.id;
+    fee = selected.proposedFee;
     // this could be specified separately by another call to use a different address, right now it is useless
     iotNode = deliverer;
-    delete candidates;
-    state = State.Agreed;
-    DelivererSelected(deliverer);
+    DelivererSelected(deliverer, fee);
+    if (this.balance >= fee) {
+      state = State.Funded;
+      ContractFunded();
+    } else {
+      state = State.Agreed;
+    }
   }
 
   function confirmCollection() public {
-    require(state == State.Agreed);
+    require(state == State.Funded);
     if (msg.sender == sender)
       senderConfirmed = true;
     else if (msg.sender == deliverer)
@@ -142,18 +159,20 @@ contract PePaDe {
       state = State.Paid;
       FeePaid();
     } else {
+      // this should really never happen, but just in case
       uint deficit = fee - this.balance;
       InsufficientFunds(deficit);
     }
   }
 
   function notifyTempBreach(int temperature) onlyIotNode public {
-    require(temperature > maxTemp);
+    require(temperature > maxTemp || temperature < minTemp);
     state = State.Exception;
     TemperatureException(temperature);
   }
 
   function garbageCollect() onlySender garbageCollectable public {
+    ContractDestroyed();
     selfdestruct(sender);
   }
 
